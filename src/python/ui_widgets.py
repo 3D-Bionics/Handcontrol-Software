e2a8a85c365ff6c0b7ab35bc9a385efeb746bdd9
zottelsheep <mail@zottelsheep.de>
2021-07-02 19:37:31 +0200

    Add new options

--- src/python/ui_widgets.py
+++ src/python/ui_widgets.py
@@ -7,7 +7,7 @@ from positions import hand_positions
 # Sliders for displaying current position of hand
 class Slider(npyscreen.SliderPercent):
     def __init__(self,screen,block_color='CAUTIONHL',*args,**keywords):
-        super(Slider,self).__init__(screen,out_of=100, step=2, editable= True, block_color=block_color ,*args,**keywords)
+        super(Slider,self).__init__(screen,out_of=100, step=2, editable= False, block_color=block_color ,*args,**keywords)
 
     def when_value_edited(self):
         self.parent.sendPos()
@@ -36,7 +36,7 @@ class BoxSelectOne(npyscreen.BoxTitle):
 class Options(npyscreen.MultiSelect):
 
     def __init__(self,screen,*args,**keywords):
-        super(Options,self).__init__(screen,values=['Loop'],*args,**keywords)
+        super(Options,self).__init__(screen,values=['Loop','Pause','Manual'],*args,**keywords)
 
     def when_value_edited(self):
         options = self.get_selected_objects() or []
@@ -46,6 +46,25 @@ class Options(npyscreen.MultiSelect):
             comframe.loop = True
         else:
             comframe.loop = False
+
+        if 'Pause' in options:
+            comframe.pause = True
+        else:
+            comframe.pause = False
+        
+        if 'Manual' in options:
+            self.parent.klein.editable = True
+            self.parent.ring.editable = True
+            self.parent.mittel.editable = True
+            self.parent.zeige.editable = True
+            self.parent.daumen.editable = True
+        else:
+            self.parent.klein.editable = False
+            self.parent.ring.editable = False
+            self.parent.mittel.editable = False
+            self.parent.zeige.editable = False
+            self.parent.daumen.editable = False
+            
 
 class BoxOptions(npyscreen.BoxTitle):
     _contained_widget = Options
